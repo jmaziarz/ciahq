87e50f1a7dc83a19bc05663345bdf9ccb4cb7a28
Jeremy Maziarz <jmaziarz@mitre.org>
2014-02-15 20:11:00 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ciahq::Application.config.secret_key_base = 'd5de7086ef5c86de6c77719e5d5eb5600ef1cc47ddfe626fdf0368f41579a9155bce5c639d7c198b9b6924f8354dfd00aea4038c18642297ad82788d1606c09c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ciahq::Application.config.session_store :cookie_store, key: '_ciahq_session'
