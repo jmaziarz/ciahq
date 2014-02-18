e51a1601755f8e4cf8aa7f7b01b5af62b6c74400
Jeremy Maziarz <jmaziarz@mitre.org>
2014-02-17 20:10:01 -0500

    Added code_name route and new logo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def codename
+    unless params[:code_name] == 'sweatpea'
+      redirect_to root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Ciahq::Application.routes.draw do
   root 'welcome#index'
+  post 'welcome/:codename' => 'welcome#codename'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
